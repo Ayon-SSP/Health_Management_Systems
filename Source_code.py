da0c5e5e3fc98193455e7748555fb4144829b35e
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2022-08-15 18:55:49 +0530

    Runned project

--- Source_code.py
+++ Source_code.py
@@ -10,14 +10,14 @@ name_of_the_clint = int(input())
 
 
 def to_do():
-    print("1)Excersise\n2)Food")
+    print("1)Exercise\n2)Food")
 
 def what_to_add_in_the_file():
-    content_of_Excersise_or_food = input()
+    content_of_Exercise_or_food = input()
     list.write("\n--------------------------------------------------\nDate and Time :-")
     list.write(str(DT))
     list.write("\n")
-    list.write(content_of_Excersise_or_food)
+    list.write(content_of_Exercise_or_food)
     list.write("\n--------------------------------------------------")
     print("UPDATE IS DONE👺")
 
@@ -25,39 +25,39 @@ def what_to_add_in_the_file():
 if (lock_or_retrieve == 1): 
     if (name_of_the_clint == 1):
         to_do()
-        food_or_excersise = int(input())
+        food_or_Exercise = int(input())
 
-        if (food_or_excersise == 1):
-            print("what would u like to save in Excersise")
-            list = open("AYON_Excersise.TXT", "a")
+        if (food_or_Exercise == 1):
+            print("what would u like to save in Exercise")
+            list = open("AYON_Exercise.TXT", "a")
             what_to_add_in_the_file()
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             print("what would u like to save in food")
             list = open("AYON_Food.TXT", "a")
             what_to_add_in_the_file()
 
     elif (name_of_the_clint == 2):
         to_do()
-        food_or_excersise = int(input())
+        food_or_Exercise = int(input())
 
-        if (food_or_excersise == 1):
-            print("what would u like to save in Excersise")
-            list = open("Hacker_Excersise.TXT", "a")
+        if (food_or_Exercise == 1):
+            print("what would u like to save in Exercise")
+            list = open("Hacker_Exercise.TXT", "a")
             what_to_add_in_the_file()
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             print("what would u like to save in food")
             list = open("Hacker_Food.TXT", "a")
             what_to_add_in_the_file()
 
     elif (name_of_the_clint == 3):
         to_do()
-        food_or_excersise = int(input())
+        food_or_Exercise = int(input())
 
-        if (food_or_excersise == 1):
-            print("what would u like to save in Excersise")
-            list = open("CARRY_Excersise.TXT", "a")
+        if (food_or_Exercise == 1):
+            print("what would u like to save in Exercise")
+            list = open("CARRY_Exercise.TXT", "a")
             what_to_add_in_the_file()
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             print("what would u like to save in food")
             list = open("CARRY_Food.TXT", "a")
             what_to_add_in_the_file()
@@ -67,33 +67,33 @@ elif (lock_or_retrieve == 2):
     if (name_of_the_clint == 1):
         to_do()
 
-        food_or_excersise = int(input())
-        if (food_or_excersise == 1):
-            list = open("AYON_Excersise.TXT")
+        food_or_Exercise = int(input())
+        if (food_or_Exercise == 1):
+            list = open("AYON_Exercise.TXT")
             print(list.read())
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             list = open("AYON_Food.TXT")
             print(list.read())
 
     elif (name_of_the_clint == 2):
         to_do()
-        food_or_excersise = int(input())
+        food_or_Exercise = int(input())
 
-        if (food_or_excersise == 1):
-            list = open("KARMAKAR_Excersise.TXT")
+        if (food_or_Exercise == 1):
+            list = open("KARMAKAR_Exercise.TXT")
             print(list.read())
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             list = open("KARMAKAR_Food.TXT")
             print(list.read())
 
     elif (name_of_the_clint == 3):
         to_do()
-        food_or_excersise = int(input())
+        food_or_Exercise = int(input())
 
-        if (food_or_excersise == 1):
-            list = open("HARRY_Excersise.TXT")
+        if (food_or_Exercise == 1):
+            list = open("HARRY_Exercise.TXT")
             print(list.read())
-        elif (food_or_excersise == 2):
+        elif (food_or_Exercise == 2):
             list = open("HARRY_Food.TXT")
             print(list.read())
   
